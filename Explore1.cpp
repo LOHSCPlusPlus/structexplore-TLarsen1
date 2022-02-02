483bc6a913cb2198c7d82c39b33feb6e2aeda230
TristanLar <>
2022-02-02 17:59:24 +0000

    Explore1 working

--- Explore1.cpp
+++ Explore1.cpp
@@ -3,22 +3,32 @@ using namespace std;
 struct Point {
     double x;
     double y;
+    
 };
 
+
+
+
+
 int main() {
-    // First instance of Point, named a
+    
     Point a;
     a.x = 3;
     a.y = 9;
-    // Second instance of Point, named b
+    
     Point b;
     b.x = 4.5;
     b.y = 7.8;
 
+    Point c;
+    c.x = 12.2;
+    c.y = 9.9;
+
     cout << "Point a: x = " << a.x << ", y = " << a.y << endl;
     cout << "Point b: x = " << b.x << ", y = " << b.y << endl;
+    cout << "Point c: x = " << c.x << ", y = " << c.y << endl;
 
-    // Add your changes here.
+
 
     return 0;
 }
